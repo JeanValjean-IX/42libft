4fbc3926a5f4532a627d5648947f0939b0db11e7
Jean Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2022-11-04 22:46:31 +0100

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blopez-f <blopez-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 20:36:18 by blopez-f          #+#    #+#             */
-/*   Updated: 2022/11/04 20:37:33 by blopez-f         ###   ########.fr       */
+/*   Created: 2022/11/02 20:17:03 by blopez-f          #+#    #+#             */
+/*   Updated: 2022/11/04 22:42:15 by blopez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	void	*buffer;
+	char	*aux_ret;
+	size_t	aux_size;
 
-	buffer = (void *)malloc(nmemb * size);
-	if (!buffer)
-		return (NULL);
-	ft_bzero(buffer, nmemb * size);
-	return (buffer);
+	if (!s)
+		return (0);
+	aux_size = ft_strlen(s);
+	if ((aux_size <= start) || len == 0)
+		return (ft_strdup(""));
+	if ((aux_size - start) < len)
+		len = aux_size - start;
+	aux_ret = ft_calloc(1, len + 1);
+	if (!aux_ret)
+		return (0);
+	return (ft_memcpy(aux_ret, s + start, len));
 }
