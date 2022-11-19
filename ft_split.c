583f37b391483b2bcbc5be3417b513a6319db4ca
Jean Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2022-11-19 11:02:02 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,25 @@
 /*   By: blopez-f <blopez-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:12:25 by blopez-f          #+#    #+#             */
-/*   Updated: 2022/11/18 23:39:46 by blopez-f         ###   ########.fr       */
+/*   Updated: 2022/11/19 10:57:49 by blopez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-size_t	ft_count_words(const char *s, char c)
+char	**ft_clean(char **buffer, const size_t j)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= j)
+		free(buffer[i++]);
+	free(buffer);
+	return (NULL);
+}
+
+size_t	ft_cwords(const char *s, char c)
 {
 	size_t	size;
 	size_t	position;
@@ -52,11 +63,10 @@ char	**ft_split(char const *s, char c)
 	char	**buffer;
 	size_t	i;
 	size_t	j;
-	size_t	k;
 
 	if (!s)
 		return (NULL);
-	buffer = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
+	buffer = (char **)ft_calloc((ft_cwords(s, c) + 1), sizeof(char *));
 	if (!buffer)
 		return (NULL);
 	i = 0;
@@ -67,18 +77,12 @@ char	**ft_split(char const *s, char c)
 			i++;
 		if (s[i])
 		{
-			buffer[j] = (char *)malloc((ft_wlen(s, c, i) + 1) * sizeof(char));
-			ft_bzero(buffer[j], (ft_wlen(s, c, i) + 1));
-			k = 0;
-			while (s[i] && s[i] != c)
-			{
-				buffer[j][k] = s[i];
-				i++;
-				k++;
-			}
+			buffer[j] = ft_substr(s, i, ft_wlen(s, c, i));
+			if (buffer[j] == NULL)
+				return (ft_clean(buffer, j));
+			i = i + ft_wlen(s, c, i);
 			j++;
 		}
 	}
-	buffer[j] = 0;
 	return (buffer);
 }
